944f6a1ca775d509634878a2ab754517f7e74e80
olamide828 <adegboyega00001@gmail.com>
2026-03-06 04:32:55 -0800

    made username not unique

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true, },
     password: { type: String, required: true },
   },
   { timestamps: true },
