4f6a727639001cb646b54c3598b5a3a0728d32d8
olamide828 <adegboyega00001@gmail.com>
2026-03-06 04:08:12 -0800

    sparse email

--- model/user.js
+++ model/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    email: { type: String, unique: true },
+    email: { type: String, unique: true, sparse: true },
     password: { type: String, required: true },
   },
   { timestamps: true },
