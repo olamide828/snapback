fae51b34ae30d50d17fb30f74726512178dab38d
olamide828 <adegboyega00001@gmail.com>
2026-03-14 05:36:47 -0700

    fixed chema error

--- model/user.js
+++ model/user.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, },
+    username: { type: String, default: "" }, 
     password: { type: String, required: true },
-    phoneNumber: {type: String, },
+    phoneNumber: { type: String, default: "" },
   },
   { timestamps: true },
 );
