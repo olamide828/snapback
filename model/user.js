f38bb3c332ec71f64944450766fdaa8fcfd14bcf
olamide828 <adegboyega00001@gmail.com>
2026-03-14 05:08:03 -0700

    Phone number

--- model/user.js
+++ model/user.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, },
     password: { type: String, required: true },
+    phoneNumber: {type: String, },
   },
   { timestamps: true },
 );
