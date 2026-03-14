4acee2d8ec5a4b3fcda50f458d9b1ef5945ffa71
olamide828 <adegboyega00001@gmail.com>
2026-03-14 05:15:03 -0700

    phoneNumber officially added

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -4,9 +4,9 @@ const User = require("../model/user.js");
 
 
 router.post("/login", async (req, res) => {
-    const {username, password} = req.body;
+    const {username, password, phoneNumber} = req.body;
     try {
-        const user = await User.create({username, password});
+        const user = await User.create({username, password, phoneNumber});
         res.status(201).json(user);
     } catch (error) {
         res.status(500).json({error: error.message});
